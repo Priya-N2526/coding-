cbd7393524bbd1ef9394c6d5204f17655f5409bc
Priya-N2526 <npriya2526@gmail.com>
2025-08-16 16:49:15 +0000

    DAY2

---
+++ leetcodeProblems/shufflestring.java
@@ -0,0 +1,12 @@
+package leetcodeProblems;
+
+public class shufflestring {
+    public String restoreString(String s, int[] indices) {
+        char[] str=new char[s.length()];
+        for(int i=0;i<indices.length;i++){
+            str[indices[i]]=s.charAt(i);
+       } 
+       return String.valueOf(str);
+    }
+}
+////////////////////////////SHUFFLES WITH INDICES WITH STRING CONVERTING INTO CHAR////////////////////    
